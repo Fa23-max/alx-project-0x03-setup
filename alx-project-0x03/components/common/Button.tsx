c4dc37065980cad32596c26a605dc1d1f5ceb3a2
Fa23-max <musilijose618@gmail.com>
2025-07-25 11:14:22 +0300

    prject set up 3

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,18 +1,18 @@
-interface ButtonProps {
-  buttonLabel: string
-  buttonSize?: string
-  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
-  action?: () => void
-}
+// interface ButtonProps {
+//   buttonLabel: string
+//   buttonSize?: string
+//   buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+//   action?: () => void
+// }
 
 const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
   const backgroundColorClass = buttonBackgroundColor ? {
-    red: 'bg-red-500',
-    blue: 'bg-blue-500',
-    orange: 'bg-orange-500',
-    green: 'bg-green-500',
-  }[buttonBackgroundColor] : 'bg-slate-500'
-  
+  //   red: 'bg-red-500',
+  //   blue: 'bg-blue-500',
+  //   orange: 'bg-orange-500',
+  //   green: 'bg-green-500',
+  // }[buttonBackgroundColor] : 'bg-slate-500'
+  }
   return (
     <button 
       onClick={action} 
