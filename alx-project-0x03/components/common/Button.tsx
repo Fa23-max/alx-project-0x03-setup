aab6c46fa3a55cc36e99e38c825fd30029a6860b
Fa23-max <musilijose618@gmail.com>
2025-07-25 11:01:40 +0300

    prject set up 3

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -15,14 +15,13 @@ const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: Butt
     orange: 'bg-orange-500',
     green: 'bg-green-500',
   }[buttonBackgroundColor] : 'bg-slate-500'
-
-
   return (
     <button onClick={action} className={`${backgroundColorClass} ${buttonSize} px-6 py-2 text-sm font-semibold rounded-lg hover:${backgroundColorClass}/50 transition duration-300 text-white`}>
       {buttonLabel}
     </button>
   )
 }
+
 
 export default Button;
 // ["import { ButtonProps }"]
